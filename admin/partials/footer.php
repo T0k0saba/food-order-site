b2b76159c2412277b1a4defd1b4bcb7407934026
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2022-07-26 15:27:59 +0200

    Created the Admin page's basis

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer section Starts -->
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All rights reserved, Food House. Developed By - <a href="#">Toókos Aba</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+</body>
+
+</html>
