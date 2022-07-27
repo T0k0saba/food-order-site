2879769cae8323bfbabf9e782d0b675707cffc67
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2022-07-27 14:25:54 +0200

    add table for manage-admin.php
    Somewhy, the css is not applied for the table

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -7,6 +7,24 @@ include('partials/menu.php');
     <div class="wrapper">
         <h1>Manage Admin</h1>
 
+        <table class="tbl-full ">
+            <tr>
+                <th>S.N.</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1. </td>
+                <td>Toókos Aba</td>
+                <td>Tokosaba</td>
+                <td>
+                    Update Admin
+                    Delete Admin
+                </td>
+            </tr>
+        </table>
 
     </div>
 </div>
