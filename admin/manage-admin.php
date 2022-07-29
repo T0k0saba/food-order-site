9ad5f45d4fd35dfe5982b16239ddb61944e0a662
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2022-07-29 11:36:48 +0200

    created pwd change button and add function for it

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -25,6 +25,25 @@ include('partials/menu.php');
             echo $_SESSION['update'];
             unset($_SESSION['update']);
         }
+
+        if(isset($_SESSION['user-not-found']))
+        {
+            echo $_SESSION['user-not-found'];
+            unset($_SESSION['user-not-found']);
+        }
+
+        if(isset($_SESSION['pwd-not-match']))
+        {
+            echo $_SESSION['pwd-not-match'];
+            unset($_SESSION['pwd-not-match']);
+        }
+
+        if(isset($_SESSION['change-pwd']))
+        {
+            echo $_SESSION['change-pwd'];
+            unset($_SESSION['change-pwd']);
+        }
+        
         ?>
         <br><br><br>
 
@@ -74,6 +93,7 @@ include('partials/menu.php');
                             <td><?php echo $full_name; ?></td>
                             <td><?php echo $username; ?></td>
                             <td>
+                                <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
                                 <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                                 <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                             </td>
