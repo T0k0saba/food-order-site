40551309f60c73695ee79f77eed9d5eefb4974d1
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2022-11-22 16:25:35 +0100

    start backend

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All rights reserved, Some Restaurant. Developed By - <a href="#">Toókos Aba</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+</body>
+
+</html>
