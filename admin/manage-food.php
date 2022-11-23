274d7d485a19ce75ab9e1de59c47189399665a2e
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2022-11-23 15:20:58 +0100

    make sql querys

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -3,6 +3,54 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Food</h1>
+        <br>
+        <br>
+
+        <!-- Button to Add Food -->
+
+        <a href="#" class="btn-primary">Add Food</a>
+        <br>
+        <br>
+        <br>
+
+        <table class="tbl-full">
+            <tr>
+                <th>S.N.</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Toókos Aba</td>
+                <td>Tokosaba</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Food</a>
+                    <a href="#" class="btn-danger">Delete Food</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2.</td>
+                <td>Toókos Aba</td>
+                <td>Tokosaba</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Food</a>
+                    <a href="#" class="btn-danger">Delete Food</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Toókos Aba</td>
+                <td>Tokosaba</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Food</a>
+                    <a href="#" class="btn-danger">Delete Food</a>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
 
