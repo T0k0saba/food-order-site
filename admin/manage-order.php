445d4fc4174f048c64b89177af2f043d1d831d4e
T0k0saba <108471404+T0k0saba@users.noreply.github.com>
2023-01-28 12:38:45 +0100

    completed

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -7,43 +7,120 @@
         <br>
         <br>
 
+        <?php
+        
+        if (isset($_SESSION['update']))
+        {
+            echo $_SESSION['update'];
+            unset($_SESSION['update']);
+        }
+
+        ?>
+        <br>
+        <br>
+
         <table class="tbl-full">
             <tr>
                 <th>S.N.</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Food</th>
+                <th>Price</th>
+                <th>Qty.</th>
+                <th>Total</th>
+                <th>Order Date</th>
+                <th>Status</th>
+                <th>Customer Name</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Address</th>
                 <th>Actions</th>
             </tr>
 
-            <tr>
-                <td>1.</td>
-                <td>Toókos Aba</td>
-                <td>Tokosaba</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            <?php
+            
+            // get all the orders from database
+            $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; // display the latest order at first
 
-            <tr>
-                <td>2.</td>
-                <td>Toókos Aba</td>
-                <td>Tokosaba</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            //execute query
+            $res = mysqli_query($conn, $sql);
 
-            <tr>
-                <td>3.</td>
-                <td>Toókos Aba</td>
-                <td>Tokosaba</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            //count the rows
+            $count = mysqli_num_rows($res);
+
+            $sn = 1; // create a serial number and set its initail value 1
+
+            if ($count > 0)
+            {
+                //order available
+                while ($row = mysqli_fetch_assoc($res))
+                {
+                    // get all the order details
+                    $id = $row['id'];
+                    $food = $row['food'];
+                    $price = $row['price'];
+                    $qty = $row['qty'];
+                    $total = $row['total'];
+                    $order_date = $row['order_date'];
+                    $status = $row['status'];
+                    $customer_name = $row['customer_name'];
+                    $customer_contact = $row['customer_contact'];
+                    $customer_email = $row['customer_email'];
+                    $customer_address = $row['customer_address'];
+
+                    ?>
+
+                        <tr>
+                            <td><?php echo $sn++; ?>.</td>
+                            <td><?php echo $food; ?></td>
+                            <td><?php echo $price; ?></td>
+                            <td><?php echo $qty; ?></td>
+                            <td><?php echo $total; ?></td>
+                            <td><?php echo $order_date; ?></td>
+
+                            <td>
+                                <?php
+                                    // ordered, on delivery, delivered, cancelled
+                                    if ($status=="Ordered")
+                                    {
+                                        echo "<label>$status</label>";
+                                    }
+                                    elseif($status=="On Delivery")
+                                    {
+                                        echo "<label style='color: orange;'>$status</label>";
+                                    }
+                                    elseif($status=="Delivered")
+                                    {
+                                        echo "<label style='color: green;'>$status</label>";
+                                    }
+                                    elseif($status=="Cancelled")
+                                    {
+                                        echo "<label style='color: red;'>$status</label>";
+                                    }
+                                 ?>
+                            </td>
+
+                            <td><?php echo $customer_name; ?></td>
+                            <td><?php echo $customer_contact; ?></td>
+                            <td><?php echo $customer_email; ?></td>
+                            <td><?php echo $customer_address; ?></td>
+                            <td>
+                                <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Order</a>
+                            </td>
+                        </tr>
+
+                    <?php
+                }
+            }
+            else
+            {
+                // order not available
+                echo "<tr><td colspan='12' class='error'>Orders not available.</td></tr>";
+            }
+            
+            ?>
+
+
+
+            
         </table>
     </div>
 </div>
--- index.php
+++ index.php
@@ -13,6 +13,17 @@
     </section>
     <!-- fOOD sEARCH Section Ends Here -->
 
+    <?php 
+
+        if (isset($_SESSION['order']))
+        {
+            echo $_SESSION['order'];
+            unset ($_SESSION['order']);
+        }
+
+
+    ?>
+
     <!-- CAtegories Section Starts Here -->
     <section class="categories">
         <div class="container">
@@ -142,7 +153,7 @@
                                     </p>
                                     <br>
 
-                                    <a href="order.php" class="btn btn-primary">Order Now</a>
+                                    <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
                                 </div>
                             </div>
 
